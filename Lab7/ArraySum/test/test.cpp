809ac9921b4a9e638d6b906be1aedf3312e330a4
Reywam <reywam@mail.ru>
2016-12-18 14:33:13 +0300

    Добавлены тесты на пустой массив

--- Lab7/ArraySum/test/test.cpp
+++ Lab7/ArraySum/test/test.cpp
@@ -27,4 +27,19 @@ BOOST_AUTO_TEST_SUITE(ArraySum_function)
 		double expectedSum = 1.9;
 		BOOST_CHECK_EQUAL(sum, expectedSum);
 	}
+
+	BOOST_AUTO_TEST_CASE(returns_default_type_value_when_array_is_empty)
+	{
+		std::vector<int> testData1;
+		int sum1 = ArraySum(testData1);
+		BOOST_CHECK_EQUAL(sum1, 0);
+
+		std::vector<std::string> testData2;
+		std::string sum2 = ArraySum(testData2);
+		BOOST_CHECK_EQUAL(sum2, "");
+
+		std::vector<double> testData3;
+		double sum3 = ArraySum(testData3);
+		BOOST_CHECK_EQUAL(sum3, 0.0);
+	}
 BOOST_AUTO_TEST_SUITE_END()
